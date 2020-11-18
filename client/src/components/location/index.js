0e7bfe2361a164004894cefdc3099ef0b702feb3
AnthonyStembreit <stembreit@gmail.com>
2020-11-18 15:20:51 -0600

    embedded a map using google maps api

--- client/src/components/location/index.js
+++ client/src/components/location/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Map from '../map'
+
+const location = {
+  address: '332 W 75th St, Kansas City, MO 64114',
+  lat: 38.992692,
+  lng: -94.593427,
+  
+}
 
 export default function Location( ){
     return(
         <div id="location">
-          location section
+          <Map location={location} zoomLevel={19} />
         </div>
     )
 }
