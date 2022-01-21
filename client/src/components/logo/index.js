36d90e63f206fd8b1d73ee62b74702156556da17
AnthonyStembreit <stembreit@gmail.com>
2022-01-21 09:31:36 -0600

    css

--- client/src/components/logo/index.js
+++ client/src/components/logo/index.js
@@ -6,7 +6,7 @@ export default function Logo(){
     return(
        
             <Jumbotron id="logo">
-                <img id="logoimg" alt="salon logo" src="https://scontent.fmkc1-1.fna.fbcdn.net/v/t31.0-8/22459446_293779794361443_678799294940314683_o.jpg?_nc_cat=107&ccb=2&_nc_sid=09cbfe&_nc_ohc=snuzwefW4aIAX8wSWZT&_nc_ht=scontent.fmkc1-1.fna&oh=e492fbb3a2acb14cd6114e0459f14255&oe=5FD6BE7B"></img>
+                <img id="logoimg" alt="salon logo" src="https://scontent.fmkc1-1.fna.fbcdn.net/v/t31.0-8/22459446_293779794361443_678799294940314683_o.jpg?_nc_cat=107&ccb=3&_nc_sid=09cbfe&_nc_ohc=hPYrnePGmJsAX8ZhcEc&_nc_ht=scontent.fmkc1-1.fna&oh=c42661e5a8bef1d58b0aadbc8b7b4679&oe=605159FB"></img>
                 <br></br>
                 <pre id="jumboLogo"> Him.    Her.    Them. </pre>
                 <p id="brandStatement">"Dolore aliqua ipsum enim proident sunt commodo ad amet quis. Dolore aliqua ipsum enim proident sunt commodo ad amet quis.Dolore aliqua ipsum enim proident sunt commodo ad amet quis."</p>
